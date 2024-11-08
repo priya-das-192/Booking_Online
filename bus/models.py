908a9f1b259d95a2894b921e99b6e6d39d34dad9
Priya <22101192@uap-bd.edu>
2024-11-09 00:57:18 +0600

    run

--- bus/models.py
+++ bus/models.py
@@ -1,3 +1,14 @@
+from os import times
+
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import DateTimeField
+
 
 # Create your models here.
+
+class Bus(models.Model):
+    name = models.CharField(max_length=200)
+    number= models.IntegerField(blank=True,null=True)
+    destination = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True ,auto_now=False)
