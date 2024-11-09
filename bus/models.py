f95cb2c5b22fadd7903d334eea1296a89efc6843
Priya <22101192@uap-bd.edu>
2024-11-09 23:32:37 +0600

    pppp

--- bus/models.py
+++ bus/models.py
@@ -24,5 +24,6 @@ class Bus(models.Model):
 
 class Seat(models.Model):
     choice= models.CharField(max_length=200,blank=True,null=True)
+    image = models.ImageField(upload_to='bus/Seat', blank=True, null=True)
     def __str__(self):
         return self.choice
